f1c58bd120c602aaa67097c86b528978865f5dc7
Deyverson1 <deyversongp@gmail.com>
2023-10-16 15:38:45 -0500

    Done: Diviendo componentes

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,16 +1,13 @@
 import './Main.css'
+import Video from './video/Main-Video'
+import Cards from './cards/Cards'
+
 export default function Main() {
+
     return(
-       <main className='main-content'>
-         <video className='main-video' src="/media/videoDropi.mp4" autoPlay muted playsInline loop></video>
-         <div className='main-video-text-container'>
-            <h2 className='main-video-lema'>El mejor aliado logístico para</h2>
-            <h2 className='main-video-lema-type'>Tu E-commerce</h2>
-            <h2 className='main-video-action'> <span>Plataforma líder</span> en <span>soluciones tecnológicas
-               </span></h2>
-            <h2 className='main-video-action'>para tu <span>negocio online
-               </span> </h2>
-         </div>
-       </main>
-    )
+      <main>
+         <Video />
+         <Cards />
+      </main>
+   )
 }
