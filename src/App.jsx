266f6e399c5f88eae8585ca83ea1885f599f4871
Deyverson1 <deyversongp@gmail.com>
2023-10-17 13:04:21 -0500

    Done: Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <Header />
       <Main />
+      <Footer />
     </>
    
   )
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,46 @@
+import './Footer.css'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faTiktok } from '@fortawesome/free-brands-svg-icons';
+
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+export default function Footer(){
+    return(
+        <footer className='footer-container'>
+            <main className='footer-links'>
+            <img className='footer-image' src="https://dropi.co/wp-content/uploads/2023/05/logo_dropi_light.png" alt="" />
+            <div className='footer-links-us'>
+                <a href="#">Contacto</a>
+                <a href="#">Capacitaciones Dropi</a>
+                <a href="#">Proveedores Premiun</a>
+            </div>
+            <div className='footer-links-terms'>
+                <a href="#">Privacidad</a>
+                <a href="#">Términos y Condiciones</a>
+            </div>
+            <div className='footer-links-contact'>
+                <a href="#">(+57) 321 8379821</a>
+                <a href="#">comercial@dropi.co</a>
+                <a href="#">Trabaja con Nosotros</a>
+            </div>
+            </main>
+           <div className='footer-final-section'>
+                <div>
+                    <p className='footer-final-text'>© DROPI | TODOS LOS DERECHOS RESERVADOS | 2023</p>
+                </div>
+                <div className='footer-social-media'>
+                    <a href="https://www.facebook.com/dropi.oficial" target='blank'> <FontAwesomeIcon icon={faFacebook} size='lg' /></a>
+                    <a href="https://www.youtube.com/@dropilatam" target='blank'> <FontAwesomeIcon icon={faYoutube}  size='lg'/></a>
+                    <a href="https://www.instagram.com/dropi.co/" target='blank'> <FontAwesomeIcon icon={faInstagram}  size='lg'/></a>
+                    <a href="https://twitter.com/dropilatam" target='blank'> <FontAwesomeIcon icon={faTwitter}  size='lg'/></a>
+                    <a href="https://www.linkedin.com/in/dropi-latam-19328a237/" target='blank'> <FontAwesomeIcon icon={faLinkedin}  size='lg'/></a>
+                    <a href="https://www.tiktok.com/@dropi.latam" target='blank'> <FontAwesomeIcon icon={faTiktok}  size='lg'/></a>
+                </div>
+           </div>
+        </footer>
+    )
+}
