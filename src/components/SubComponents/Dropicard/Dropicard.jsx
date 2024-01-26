d5b67fa99c734c1b6b13450f366cc9f573b868ce
Deyverson1 <deyversongp@gmail.com>
2024-01-26 11:35:49 -0500

    Updates: Finishin Dropicard

--- src/components/SubComponents/Dropicard/Dropicard.jsx
+++ src/components/SubComponents/Dropicard/Dropicard.jsx
@@ -1,6 +1,9 @@
 import "./Dropicard.css"
 import Header from "../../Header/Header"
 import Footer from "../../Footer/Footer"
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 export default function Dropicard(){
     return (
         <>
@@ -8,14 +11,44 @@ export default function Dropicard(){
             <div className='dropicard-container'>
                 <img className='dropicard-image' src="https://dropi.co/wp-content/uploads/2023/08/dropicard.png" alt="" />
                <div className='list-container-dropicard'>
-                
                 <button className="dropiButton">dropicard</button>
                     <div className='list-dropicard'>
-                        
                         <p className='text-dropicard'>NUESTRA TARJETA DE CRÉDITO ES UNA NUEVA HERRAMIENTA FINANCIERA, DROPICARD ROMPE LA BRECHA DE AQUELLOS QUE NO ESTÁN BANCARIZADOS Y QUIEREN AGILIZAR SUS PROCESOS DE TRANSFERENCIAS DE WALLET A UN BIEN FINAL</p>
                     </div>
-                   
                </div>
+            </div>
+            <button className="beneficsButton">Beneficios</button>
+            <section className="list-container">
+                <ul>
+                    <li> <div className="slider-card"><FontAwesomeIcon icon={faCheck} style={{color: "#ec6f2d", marginRight: ".5rem", paddingTop: ".3rem"}} /> <p className='text-list'>Simplifica el proceso de activación de tus anuncios de campañas.</p></div></li>
+                    <li> <div className="slider-card"><FontAwesomeIcon icon={faCheck} style={{color: "#ec6f2d", marginRight: ".5rem", paddingTop: ".3rem"}} /> <p className='text-list'>Crea tantas tarjetas como necesites.</p></div></li>
+                    <li> <div className="slider-card"><FontAwesomeIcon icon={faCheck} style={{color: "#ec6f2d", marginRight: ".5rem", paddingTop: ".3rem"}} /> <p className='text-list'>Recarga tu Dropi Card con el saldo de tu Wallet en un solo clic.</p></div></li>
+                   
+                </ul>
+                <ul>
+                     <li> <div className="slider-card"><FontAwesomeIcon icon={faCheck} style={{color: "#ec6f2d", marginRight: ".5rem", paddingTop: ".3rem"}} /> <p className='text-list'>Puedes hacer múltiples movimientos en un mismo día.</p></div></li>
+                    <li> <div className="slider-card"><FontAwesomeIcon icon={faCheck} style={{color: "#ec6f2d", marginRight: ".5rem", paddingTop: ".3rem"}} /> <p className='text-list'>Tu tarjeta aliada con VISA.</p></div></li>
+                    <li> <div className="slider-card"><FontAwesomeIcon icon={faCheck} style={{color: "#ec6f2d", marginRight: ".5rem", paddingTop: ".3rem"}} /> <p className='text-list'>Compra online, sin límites.</p></div></li>
+            
+                </ul>
+            </section>
+            <section className="textFullContainer">
+                <h2 className="textTitle">¿Qué podrás</h2>
+                <div className="containerText">
+                    <h3 className="textDropi">Encontrar?</h3>
+                </div>
+            </section>
+            <div className="imageDiv">
+                <img src="https://dropi.co/wp-content/uploads/2023/08/ICONOS-PAGINA-1536x412.png" alt="" />
+            </div>
+            <section className="secondTextFullContainer">
+                <h2 className="secondTextTitle">Así de fácil</h2>
+                <div className="secondContainerText">
+                    <h3 className="secondTextDropi">La obtienes</h3>
+                </div>
+            </section>
+            <div className="infographic">
+                <img src="https://dropi.co/wp-content/uploads/2023/08/Infografia_Horizontal-2-1536x474.png" alt="" />
             </div>
             <Footer/>
         </>
